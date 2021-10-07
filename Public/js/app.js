98926d24c901f41028a0fe10840698a1a67d2178
Samed Walker <samedwalker50@gmail.com>
2021-10-06 22:51:59 -0400

    Deleted unnecessary console.log call

--- Public/js/app.js
+++ Public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Client side js file is loaded");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
@@ -11,7 +9,7 @@ weatherForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const location = search.value;
-  messageOne.textContent = "Loading......"
+  messageOne.textContent = "Loading......";
   messageTwo.textContent = "";
   fetch(`http://localhost:3000/weather?address=${location}`)
     .then((response) => response.json())
